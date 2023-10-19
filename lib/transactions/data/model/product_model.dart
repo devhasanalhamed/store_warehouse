9bc2fc4998a1435375883ad9990386bcb5d92f1e
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-19 11:11:10 +0300

    Create product_model.dart

---
+++ lib/transactions/data/model/product_model.dart
@@ -0,0 +1,11 @@
+import 'package:store_warehouse/transactions/domain/entity/product.dart';
+
+class ProductModel extends Product {
+  const ProductModel({
+    required super.id,
+    required super.title,
+    required super.category,
+    required super.unit,
+    required super.quantity,
+  });
+}
