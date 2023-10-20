e11cf5f7fac2e52f91e6b2fd66122da08431cecc
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-20 17:08:25 +0300

    Create transactions_widget.dart

---
+++ lib/transactions/transactions_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:store_warehouse/products/products_provider.dart';
+import 'package:store_warehouse/transactions/data/model/transaction.model.dart';
+
+class TransactionsWidget extends StatelessWidget {
+  final TransactionModel transaction;
+  const TransactionsWidget({Key? key, required this.transaction})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Text(Provider.of<ProductsProvider>(context, listen: false)
+            .products
+            .firstWhere((element) => element.id == transaction.productId)
+            .title),
+        title: RichText(
+          text: TextSpan(
+            text: transaction.quantity.toString(),
+            style: const TextStyle(color: Colors.black),
+            children: const [
+              TextSpan(text: 'حبة'),
+            ],
+          ),
+        ),
+        trailing: Text(
+          DateFormat.yMd().format(transaction.date),
+        ),
+      ),
+    );
+  }
+}
