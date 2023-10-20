069ac8b55e832838e5cd94d7b014ece4bb8d2309
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-20 17:08:09 +0300

    Update product.dart

--- lib/transactions/domain/entity/product.dart
+++ lib/transactions/domain/entity/product.dart
@@ -5,16 +5,16 @@ class Product extends Equatable {
   final int id;
   final String title;
   final String category;
-  final Unit unit;
+  final int unitId;
   final int quantity;
 
   const Product({
     required this.id,
     required this.title,
     required this.category,
-    required this.unit,
+    required this.unitId,
     required this.quantity,
   });
   @override
-  List<Object> get props => [id, title, category, unit, quantity];
+  List<Object> get props => [id, title, category, unitId, quantity];
 }
