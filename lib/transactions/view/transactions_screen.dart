42c5671f83b7be01cc0013dd5bace68717c1304b
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-25 20:27:02 +0300

    Create transactions_screen.dart

---
+++ lib/transactions/view/transactions_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_warehouse/core/shared/products_transactions_provider.dart';
+import 'package:store_warehouse/transactions/view/transactions_widget.dart';
+
+class TransactionsScreen extends StatefulWidget {
+  const TransactionsScreen({Key? key}) : super(key: key);
+
+  @override
+  TransactionsScreenState createState() => TransactionsScreenState();
+}
+
+class TransactionsScreenState extends State<TransactionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final transaction =
+        Provider.of<ProductsTransactionsProvider>(context).transactions;
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: transaction.length,
+            itemBuilder: (context, index) => TransactionsWidget(
+              transaction: transaction[index],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
