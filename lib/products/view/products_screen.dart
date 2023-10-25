9b4243d3dbbc64b3117928e08eb03acb3bb38771
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-25 20:26:34 +0300

    Create products_screen.dart

---
+++ lib/products/view/products_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_warehouse/core/shared/products_transactions_provider.dart';
+import 'package:store_warehouse/products/view/product_widget.dart';
+
+class ProductsScreen extends StatefulWidget {
+  const ProductsScreen({Key? key}) : super(key: key);
+
+  @override
+  ProductsScreenState createState() => ProductsScreenState();
+}
+
+class ProductsScreenState extends State<ProductsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final products =
+        Provider.of<ProductsTransactionsProvider>(context).products;
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Expanded(
+          child: ListView.builder(
+            itemCount: products.length,
+            itemBuilder: (context, index) =>
+                ProductWidget(product: products[index]),
+          ),
+        ),
+      ],
+    );
+  }
+}
