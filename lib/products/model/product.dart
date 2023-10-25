98c2e490d0766d7ab5cecd7bc18fe9edcb4f8662
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-25 20:25:48 +0300

    Create product.dart

---
+++ lib/products/model/product.dart
@@ -0,0 +1,18 @@
+
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final int unitId;
+  final int quantity;
+  final int totalAmount;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.unitId,
+    required this.quantity,
+    required this.totalAmount,
+  });
+}
