0cc3086e2529d7aebdf49fb02dde31644f0002f3
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-25 20:26:51 +0300

    Create transaction.model.dart

---
+++ lib/transactions/model/transaction.model.dart
@@ -0,0 +1,9 @@
+
+// class TransactionModel extends TransAction {
+//   TransactionModel({
+//     required super.id,
+//     required super.quantity,
+//     required super.productId,
+//     required super.date,
+//   });
+// }
