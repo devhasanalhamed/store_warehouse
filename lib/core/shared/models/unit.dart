f52267f68ac6fd60351889864597a8143c72368d
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-25 21:10:42 +0300

    Create unit.dart

---
+++ lib/core/shared/models/unit.dart
@@ -0,0 +1,13 @@
+
+class Unit   {
+  final int id;
+  final String title;
+  final int unitPerPiece;
+
+   Unit({
+    required this.id,
+    required this.title,
+    required this.unitPerPiece,
+  });
+
+}
