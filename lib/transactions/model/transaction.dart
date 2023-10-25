d0f51f7994ed9c656715f49b654a903fc85dc573
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-25 20:26:38 +0300

    Create transaction.dart

---
+++ lib/transactions/model/transaction.dart
@@ -0,0 +1,13 @@
+class TransAction {
+  final int id;
+  final int quantity;
+  final int productId;
+  final DateTime date;
+
+  TransAction({
+    required this.id,
+    required this.quantity,
+    required this.productId,
+    required this.date,
+  });
+}
