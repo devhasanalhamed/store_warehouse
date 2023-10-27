c4c3947b82b9d520ce8a53d1824516d2ddc17029
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-27 11:13:33 +0300

    Update unit_provider.dart

--- lib/core/shared/models/unit_provider.dart
+++ lib/core/shared/models/unit_provider.dart
@@ -1,22 +1,31 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:store_warehouse/core/shared/models/unit.dart';
+import 'package:store_warehouse/core/sql_helper.dart';
 
 class UnitProvider extends ChangeNotifier {
-  final List<Unit> list = [
-    Unit(id: 0, title: 'بالحبة', unitPerPiece: 1),
-    Unit(id: 1, title: 'كرتون 24', unitPerPiece: 24),
-    Unit(id: 2, title: 'كرتون 12', unitPerPiece: 12),
+  List<Unit> list = [
+    // Unit(id: 0, title: 'بالحبة', unitPerPiece: 1),
+    // Unit(id: 1, title: 'كرتون 24', unitPerPiece: 24),
+    // Unit(id: 2, title: 'كرتون 12', unitPerPiece: 12),
   ];
 
-  Future<void> addUnit(String title, int unitPerPiece) async {
-    list.add(
-      Unit(
-        id: Random().nextInt(5000) + 2,
-        title: title,
-        unitPerPiece: unitPerPiece,
-      ),
-    );
+  Future<int> addUnit(String title, int unitPerPiece) async {
+    return await SQLHelper.createUnit(title, unitPerPiece);
+  }
+
+  Future<void> getUnits() async {
+    List<Unit> temp = [];
+    final dbList = await SQLHelper.getUnits();
+    for (var element in dbList) {
+      temp.add(
+        Unit(
+          id: element['id'],
+          title: element['title'],
+          unitPerPiece: element['unitPerPiece'],
+        ),
+      );
+      list = temp;
+    }
+    notifyListeners();
   }
 }
