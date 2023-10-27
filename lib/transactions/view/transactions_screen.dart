f29c207a6ee3cec2676c54d12f2fa2e2de3da455
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-27 11:13:48 +0300

    Update transactions_screen.dart

--- lib/transactions/view/transactions_screen.dart
+++ lib/transactions/view/transactions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/core/shared/models/products_transactions_provider.dart';
+import 'package:store_warehouse/transactions/model/transaction.dart';
 import 'package:store_warehouse/transactions/view/transactions_widget.dart';
 
 class TransactionsScreen extends StatefulWidget {
@@ -13,19 +14,21 @@ class TransactionsScreen extends StatefulWidget {
 class TransactionsScreenState extends State<TransactionsScreen> {
   @override
   Widget build(BuildContext context) {
-    final transaction =
-        Provider.of<ProductsTransactionsProvider>(context).transactions;
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: transaction.length,
+    return FutureBuilder<List<TransAction>>(
+      future:
+          Provider.of<ProductsTransactionsProvider>(context).getTransactions(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
             itemBuilder: (context, index) => TransactionsWidget(
-              transaction: transaction[index],
+              transaction: snapshot.data![index],
             ),
-          ),
-        ),
-      ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
