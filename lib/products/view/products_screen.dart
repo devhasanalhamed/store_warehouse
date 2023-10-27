e78da6a05260500e6831942de5d09420a525eac3
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-27 11:13:42 +0300

    Update products_screen.dart

--- lib/products/view/products_screen.dart
+++ lib/products/view/products_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/core/shared/models/products_transactions_provider.dart';
+import 'package:store_warehouse/core/shared/models/unit_provider.dart';
+import 'package:store_warehouse/products/model/product.dart';
 import 'package:store_warehouse/products/view/product_widget.dart';
 
 class ProductsScreen extends StatefulWidget {
@@ -13,19 +15,27 @@ class ProductsScreen extends StatefulWidget {
 class ProductsScreenState extends State<ProductsScreen> {
   @override
   Widget build(BuildContext context) {
-    final products =
-        Provider.of<ProductsTransactionsProvider>(context).products;
-    return Column(
-      children: [
-        const SizedBox(height: 16.0),
-        Expanded(
-          child: ListView.builder(
-            itemCount: products.length,
-            itemBuilder: (context, index) =>
-                ProductWidget(product: products[index]),
-          ),
-        ),
-      ],
+    Provider.of<UnitProvider>(context).getUnits();
+    return FutureBuilder<List<Product>>(
+      future: Provider.of<ProductsTransactionsProvider>(context).getProduct(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Column(
+            children: [
+              const SizedBox(height: 16.0),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) =>
+                      ProductWidget(product: snapshot.data![index]),
+                ),
+              ),
+            ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
