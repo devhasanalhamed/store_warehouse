58cb3ea46c4747b89a597a212aa947bb6bc827c5
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-30 10:42:33 +0300

    Update product_widget.dart
    add new design

--- lib/products/view/product_widget.dart
+++ lib/products/view/product_widget.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/core/shared/models/products_transactions_provider.dart';
+import 'package:store_warehouse/core/shared/models/unit.dart';
 import 'package:store_warehouse/core/shared/models/unit_provider.dart';
 import 'package:store_warehouse/products/model/product.dart';
 import 'package:store_warehouse/transactions/model/transaction.dart';
@@ -39,6 +42,7 @@ class ProductWidget extends StatelessWidget {
               subtitle: Text(product.description),
               trailing: IconButton(
                 icon: const Icon(Icons.delete),
+                color: const Color.fromARGB(255, 235, 102, 92),
                 onPressed: () {
                   // Provider.of<ProductsTransactionsProvider>(context,
                   //         listen: false)
@@ -47,17 +51,25 @@ class ProductWidget extends StatelessWidget {
               ),
             ),
             Table(
+              border: const TableBorder(
+                horizontalInside: BorderSide(color: Colors.grey, width: 1),
+                verticalInside: BorderSide(color: Colors.grey, width: 1),
+                bottom: BorderSide(color: Colors.grey, width: 1),
+              ),
               children: [
                 const TableRow(
                   children: [
                     Text(
                       'نوع الوحدة',
+                      textAlign: TextAlign.center,
                     ),
                     Text(
                       'الكمية بالوحدة',
+                      textAlign: TextAlign.center,
                     ),
                     Text(
                       'الكمية بالحبة',
+                      textAlign: TextAlign.center,
                     ),
                   ],
                 ),
@@ -68,12 +80,15 @@ class ProductWidget extends StatelessWidget {
                           .list
                           .firstWhere((element) => element.id == product.unitId)
                           .title,
+                      textAlign: TextAlign.center,
                     ),
                     Text(
                       product.quantity.toString(),
+                      textAlign: TextAlign.center,
                     ),
                     Text(
                       (product.totalAmount).toString(),
+                      textAlign: TextAlign.center,
                     ),
                   ],
                 ),
@@ -88,94 +103,205 @@ class ProductWidget extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 8),
-            Row(
-              children: [
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {
-                    int quantity = 0;
-                    final value = Provider.of<ProductsTransactionsProvider>(
-                        context,
-                        listen: false);
-                    showModalBottomSheet(
-                      context: context,
-                      builder: (context) => Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: Container(
-                          width: 350,
-                          height: 350,
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 8.0,
-                            vertical: 16.0,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Row(
-                                children: [
-                                  const Text(
-                                    'نوع الوحدة',
-                                  ),
-                                  const SizedBox(width: 12.0),
-                                  Expanded(
-                                    child: DropdownButtonFormField(
-                                      value: null,
-                                      decoration: InputDecoration(
-                                        border: const OutlineInputBorder(),
-                                        hintText: product.unitId.toString(),
-                                        contentPadding:
-                                            const EdgeInsets.symmetric(
-                                          horizontal: 8.0,
-                                          vertical: 4.0,
-                                        ),
-                                      ),
-                                      padding: EdgeInsets.zero,
-                                      items: const [],
-                                      onChanged: null,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: [
+                  ElevatedButton.icon(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      int quantity = 0;
+                      final value = Provider.of<ProductsTransactionsProvider>(
+                          context,
+                          listen: false);
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: Container(
+                            width: 350,
+                            height: 350,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0,
+                              vertical: 16.0,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'نوع الوحدة',
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'الكمية',
-                                  ),
-                                  const SizedBox(width: 12.0),
-                                  Expanded(
-                                    child: TextFormField(
-                                      decoration: const InputDecoration(
-                                        border: OutlineInputBorder(),
-                                        hintText: 'أدخل الكمية بالحبة',
-                                        contentPadding: EdgeInsets.symmetric(
-                                          horizontal: 8.0,
-                                          vertical: 4.0,
+                                    const SizedBox(width: 12.0),
+                                    Expanded(
+                                      child: DropdownButtonFormField(
+                                        value: null,
+                                        decoration: InputDecoration(
+                                          border: const OutlineInputBorder(),
+                                          hintText: product.unitId.toString(),
+                                          contentPadding:
+                                              const EdgeInsets.symmetric(
+                                            horizontal: 8.0,
+                                            vertical: 4.0,
+                                          ),
                                         ),
+                                        padding: EdgeInsets.zero,
+                                        items: const [],
+                                        onChanged: null,
                                       ),
-                                      onChanged: (value) =>
-                                          quantity = int.parse(value),
-                                      keyboardType: TextInputType.number,
                                     ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'الكمية',
+                                    ),
+                                    const SizedBox(width: 12.0),
+                                    Expanded(
+                                      child: TextFormField(
+                                        decoration: const InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          hintText: 'أدخل الكمية بالحبة',
+                                          contentPadding: EdgeInsets.symmetric(
+                                            horizontal: 8.0,
+                                            vertical: 4.0,
+                                          ),
+                                        ),
+                                        onChanged: (value) =>
+                                            quantity = int.parse(value),
+                                        keyboardType: TextInputType.number,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => value
+                                      .updateAddQuantity(product.id, quantity)
+                                      .then((value) => Navigator.pop(context)),
+                                  child: const Text(
+                                    'إضافة كمية جديدة',
                                   ),
-                                ],
-                              ),
-                              ElevatedButton(
-                                onPressed: () => value
-                                    .updateAddQuantity(product.id, quantity)
-                                    .then((value) => Navigator.pop(context)),
-                                child: const Text('إضافة كمية جديدة'),
-                              ),
-                            ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                  label: const Text('كمية جديدة'),
-                ),
-              ],
-              
+                      );
+                    },
+                    label: const Text('كمية جديدة'),
+                  ),
+                  const SizedBox(width: 16.0),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      String title = '';
+                      String description = '';
+                      int unitId = product.unitId;
+                      final provider =
+                          Provider.of<ProductsTransactionsProvider>(context,
+                              listen: false);
+                      final unitList =
+                          Provider.of<UnitProvider>(context, listen: false)
+                              .list;
+                      log(unitList.length.toString());
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: Container(
+                            width: 350,
+                            height: 350,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0,
+                              vertical: 16.0,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                TextFormField(
+                                  initialValue: product.title,
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 8.0,
+                                      vertical: 4.0,
+                                    ),
+                                  ),
+                                  onChanged: (value) => title = value,
+                                  keyboardType: TextInputType.number,
+                                ),
+                                TextFormField(
+                                  initialValue: product.description,
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 8.0,
+                                      vertical: 4.0,
+                                    ),
+                                  ),
+                                  onChanged: (value) => title = value,
+                                  keyboardType: TextInputType.number,
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'نوع الوحدة',
+                                    ),
+                                    const SizedBox(width: 12.0),
+                                    Expanded(
+                                      child: DropdownButtonFormField(
+                                        value: product.unitId,
+                                        decoration: const InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          contentPadding: EdgeInsets.symmetric(
+                                            horizontal: 8.0,
+                                            vertical: 4.0,
+                                          ),
+                                        ),
+                                        padding: EdgeInsets.zero,
+                                        items: [
+                                          for (var item in unitList)
+                                            DropdownMenuItem(
+                                              value: item.id,
+                                              alignment: Alignment.centerRight,
+                                              child: Text(item.title),
+                                            ),
+                                        ],
+                                        onChanged: (value) => unitId = value!,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Text(
+                                  'سيتم تعديل المعلومات على جميع العمليات السابقة',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => (),
+                                  child: const Text(
+                                    'تعديل معلومات المنتج',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    style: const ButtonStyle(
+                      foregroundColor: MaterialStatePropertyAll(Colors.green),
+                      backgroundColor: MaterialStatePropertyAll(Colors.white),
+                    ),
+                    icon: const Icon(Icons.edit),
+                    label: const Text('تعديل'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
