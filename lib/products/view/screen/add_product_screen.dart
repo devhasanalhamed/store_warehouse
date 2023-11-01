bd34f1b195923d51d308dca27e085aa042bb31c2
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-02 02:52:50 +0300

    Update add_product_screen.dart

--- lib/products/view/screen/add_product_screen.dart
+++ lib/products/view/screen/add_product_screen.dart
@@ -1,26 +1,46 @@
-import 'dart:ui';
-
+import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/core/shared/models/products_transactions_provider.dart';
-import 'package:store_warehouse/core/shared/models/unit.dart';
+
 import 'package:store_warehouse/core/shared/models/unit_provider.dart';
 import 'package:store_warehouse/core/shared/view/widget/drop_from_field_component.dart';
 import 'package:store_warehouse/core/shared/view/widget/elevated_button_component.dart';
 import 'package:store_warehouse/core/shared/view/widget/text_form_field_component.dart';
+import 'package:store_warehouse/products/controller/product_controller.dart';
 import 'package:store_warehouse/products/view/widget/product_image_picker.dart';
 
 class AddProductScreen extends StatelessWidget {
   AddProductScreen({Key? key}) : super(key: key);
-
+  final GlobalKey<FormState> formKey = GlobalKey();
   final TextEditingController titleController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
-  final Unit? unitController = null;
   final TextEditingController quantityController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    int? unitController;
+
+    void validate() {
+      final isValid = formKey.currentState!.validate();
+      if (isValid) {
+        log('Product information is valid, calling provider...');
+        Provider.of<ProductsTransactionsProvider>(context, listen: false)
+            .addProduct(
+              titleController.text,
+              descriptionController.text,
+              Provider.of<ProductController>(context, listen: false)
+                  .imagePicker!
+                  .path,
+              unitController!,
+              int.parse(quantityController.text),
+            )
+            .then((value) => Navigator.pop(context));
+      } else {
+        log('not valid');
+      }
+    }
+
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
@@ -35,96 +55,100 @@ class AddProductScreen extends StatelessWidget {
                 horizontal: 8.0,
                 vertical: 16.0,
               ),
-              child: Column(
-                children: [
-                  ProductImagePicker(
-                    onTap: () async {
-                      XFile? image = await ImagePicker()
-                          .pickImage(source: ImageSource.camera);
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextFormFieldComponent(
-                    controller: titleController,
-                    label: 'عنوان المنتج',
-                    keyboardType: TextInputType.text,
-                    onSubmit: (value) => {},
-                    validator: (value) {
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextFormFieldComponent(
-                    controller: descriptionController,
-                    label: 'وصف المنتج',
-                    keyboardType: TextInputType.text,
-                    onSubmit: (value) => {},
-                    validator: (value) {
-                      return null;
-                    },
-                    maxLines: 2,
-                  ),
-                  const SizedBox(height: 16.0),
-                  Consumer<UnitProvider>(
-                    builder: (context, value, child) =>
-                        DropDownButtonFormFieldComponent(
-                      label: 'أختر نوع الوحدة',
-                      onChanged: (value) {},
-                      dropList: [
-                        for (var unit in value.list)
-                          DropdownMenuItem(
-                            value: unit.id,
-                            alignment: Alignment.centerRight,
-                            child: Text(unit.title),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    Consumer<ProductController>(
+                      builder: (context, value, child) => ProductImagePicker(
+                        onTap: value.pickImage,
+                        photo: value.imagePicker,
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormFieldComponent(
+                      controller: titleController,
+                      label: 'عنوان المنتج',
+                      keyboardType: TextInputType.text,
+                      onSubmit: (value) => {},
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'الرجاء اختيار عنوان';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormFieldComponent(
+                      controller: descriptionController,
+                      label: 'وصف المنتج',
+                      keyboardType: TextInputType.text,
+                      onSubmit: (value) => {},
+                      validator: (value) {
+                        return null;
+                      },
+                      maxLines: 2,
+                    ),
+                    const SizedBox(height: 16.0),
+                    Consumer<UnitProvider>(
+                      builder: (context, value, child) =>
+                          DropDownButtonFormFieldComponent(
+                        label: 'أختر نوع الوحدة',
+                        onChanged: (value) =>
+                            unitController = int.parse('$value'),
+                        validator: (value) {
+                          if (unitController == null) {
+                            return 'الرجاء اختيار الوحدة';
+                          }
+                          return null;
+                        },
+                        dropList: [
+                          for (var unit in value.list)
+                            DropdownMenuItem(
+                              value: unit.id,
+                              alignment: Alignment.centerRight,
+                              child: Text(unit.title),
+                            ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 4.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () => addUnit(context),
+                            child: const Text('إضافة وحدة جديدة'),
                           ),
+                        ),
                       ],
                     ),
-                  ),
-                  const SizedBox(height: 4.0),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextButton(
-                          onPressed: () => addUnit(context),
-                          child: const Text('إضافة وحدة جديدة'),
+                    const SizedBox(height: 4.0),
+                    TextFormFieldComponent(
+                      controller: quantityController,
+                      keyboardType: TextInputType.number,
+                      onSubmit: (value) {},
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'الرجاء إدخال الكمية';
+                        }
+                        return null;
+                      },
+                      label: 'الكمية بالوحدة',
+                    ),
+                    const SizedBox(height: 16.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButtonComponent(
+                            onPressed: () => validate(),
+                            child: const Text('إضافة منتج جديد'),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 4.0),
-                  TextFormFieldComponent(
-                    controller: quantityController,
-                    keyboardType: TextInputType.number,
-                    onSubmit: (value) {},
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'الرجاء إدخال الكمية';
-                      }
-                      return null;
-                    },
-                    label: 'الكمية بالوحدة',
-                  ),
-                  const SizedBox(height: 16.0),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ElevatedButtonComponent(
-                          onPressed: () {
-                            Provider.of<ProductsTransactionsProvider>(context,
-                                    listen: false)
-                                .addProduct(
-                              titleController.text,
-                              descriptionController.text,
-                              1,
-                              int.parse(quantityController.text),
-                            );
-                          },
-                          child: const Text('إضافة منتج جديد'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
