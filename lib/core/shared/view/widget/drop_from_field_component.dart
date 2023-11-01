4efac20ffe82fa0e356f839a214715eff7b570c1
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-02 02:52:40 +0300

    Update drop_from_field_component.dart

--- lib/core/shared/view/widget/drop_from_field_component.dart
+++ lib/core/shared/view/widget/drop_from_field_component.dart
@@ -4,11 +4,13 @@ class DropDownButtonFormFieldComponent extends StatelessWidget {
   final String label;
   final List<DropdownMenuItem> dropList;
   final ValueChanged<dynamic> onChanged;
+  final FormFieldValidator<dynamic>? validator;
   const DropDownButtonFormFieldComponent({
     Key? key,
     required this.label,
     required this.dropList,
     required this.onChanged,
+    this.validator,
   }) : super(key: key);
 
   @override
@@ -24,6 +26,7 @@ class DropDownButtonFormFieldComponent extends StatelessWidget {
         label: Text(label),
         isDense: true,
       ),
+      validator: validator,
       padding: EdgeInsets.zero,
       items: dropList,
       onChanged: onChanged,
