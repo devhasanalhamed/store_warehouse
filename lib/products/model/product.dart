4262c830bb6ec5457356f4006ef0544419d3f227
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-02 02:52:48 +0300

    Update product.dart

--- lib/products/model/product.dart
+++ lib/products/model/product.dart
@@ -2,6 +2,7 @@ class Product {
   final int id;
   final String title;
   final String description;
+  final String imagePath;
   final int unitId;
   final int quantity;
   final int totalAmount;
@@ -9,6 +10,7 @@ class Product {
   Product({
     required this.id,
     required this.title,
+    required this.imagePath,
     required this.description,
     required this.unitId,
     required this.quantity,
@@ -19,6 +21,7 @@ class Product {
         id: record['id'],
         title: record['title'],
         description: record['description'],
+        imagePath: record['imagePath'],
         unitId: record['unitId'] as int,
         quantity: record['quantity'] as int,
         totalAmount: record['totalAmount'] as int,
