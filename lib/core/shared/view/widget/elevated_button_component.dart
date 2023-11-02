998726e6e25840e6bfe384adbefba83d01afdc93
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-02 21:04:23 +0300

    Update elevated_button_component.dart

--- lib/core/shared/view/widget/elevated_button_component.dart
+++ lib/core/shared/view/widget/elevated_button_component.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ElevatedButtonComponent extends StatelessWidget {
-  final Widget child;
+  final String title;
   final VoidCallback? onPressed;
   const ElevatedButtonComponent({
     Key? key,
-    required this.child,
     required this.onPressed,
+    required this.title,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      child: const Text('إضافة منتج جديد'),
+      style: ElevatedButton.styleFrom(
+        fixedSize: const Size(double.infinity, 45),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
