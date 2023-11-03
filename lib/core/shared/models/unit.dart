f1c14933d445e2cdf902263944525458ddeba584
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-03 18:15:49 +0300

    Update unit.dart

--- lib/core/shared/models/unit.dart
+++ lib/core/shared/models/unit.dart
@@ -1,13 +1,17 @@
-
-class Unit   {
+class Unit {
   final int id;
   final String title;
   final int unitPerPiece;
 
-   Unit({
+  Unit({
     required this.id,
     required this.title,
     required this.unitPerPiece,
   });
 
+  factory Unit.fromSQL(Map<String, dynamic> record) => Unit(
+        id: record['id'] as int,
+        title: record['title'],
+        unitPerPiece: record['unitPerPiece'] as int,
+      );
 }
