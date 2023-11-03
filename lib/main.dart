e258fd29fbc0d4d37afbee2e44a3bb6846304771
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-03 18:16:10 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:store_warehouse/core/shared/models/products_transactions_provider.dart';
 import 'package:store_warehouse/core/shared/models/unit_provider.dart';
 import 'package:store_warehouse/core/utils/sql_helper.dart';
 import 'package:store_warehouse/products/controller/product_controller.dart';
 import 'package:store_warehouse/products/model/product.dart';
 import 'package:store_warehouse/products/view/screen/add_product_screen.dart';
 import 'package:store_warehouse/products/view/screen/products_screen.dart';
+import 'package:store_warehouse/transactions/controller/transaction_controller.dart';
 import 'package:store_warehouse/transactions/view/transactions_screen.dart';
 
 void main() async {
@@ -25,18 +25,11 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (context) => UnitProvider(),
         ),
-        ChangeNotifierProxyProvider<UnitProvider, ProductsTransactionsProvider>(
-          create: (context) => ProductsTransactionsProvider(unitList: []),
-          update: (context, value, previous) {
-            if (value.list.length != previous!.unitList.length) {
-              return ProductsTransactionsProvider(unitList: value.list);
-            } else {
-              return previous;
-            }
-          },
-        ),
         ChangeNotifierProvider(
           create: (context) => ProductController(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => TransactionController(),
         ),
       ],
       child: MaterialApp(
@@ -45,9 +38,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Cairo',
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: Consumer<ProductsTransactionsProvider>(
-          builder: (context, value, child) => const HomePage(),
-        ),
+        home: const HomePage(),
       ),
     );
   }
@@ -85,9 +76,8 @@ class HomePageState extends State<HomePage> {
         onPressed: () {
           int productId = 0;
           int quantity = 0;
-          final x =
-              Provider.of<ProductsTransactionsProvider>(context, listen: false)
-                  .getProduct();
+          final x = Provider.of<ProductController>(context, listen: false)
+              .getProduct();
           showDialog(
             context: context,
             builder: (s) => Dialog(
@@ -157,11 +147,11 @@ class HomePageState extends State<HomePage> {
                         ],
                       ),
                       ElevatedButton(
-                        onPressed: () =>
-                            Provider.of<ProductsTransactionsProvider>(context,
-                                    listen: false)
-                                .addTransaction(productId, quantity)
-                                .then((value) => {Navigator.of(context).pop()}),
+                        onPressed: () => Provider.of<TransactionController>(
+                                context,
+                                listen: false)
+                            .addTransaction(productId, quantity)
+                            .then((value) => {Navigator.of(context).pop()}),
                         child: const Text('إضافة عملية'),
                       ),
                     ],
@@ -178,152 +168,149 @@ class HomePageState extends State<HomePage> {
 
     return Directionality(
       textDirection: TextDirection.rtl,
-      child: Consumer<ProductsTransactionsProvider>(
-        builder: (context, value, child) => Scaffold(
-          resizeToAvoidBottomInset: true,
-          drawer: Drawer(
-            child: Container(
-              color: const Color(0xFFFEFEFE),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Divider(color: Colors.deepPurple),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                      String unitTitle = '';
-                      int unitPerPiece = 0;
-                      showDialog(
-                        context: context,
-                        builder: (s) => Dialog(
-                          child: Directionality(
-                            textDirection: TextDirection.rtl,
-                            child: Container(
-                              height: 350,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0,
-                                vertical: 16.0,
-                              ),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  TextFormField(
-                                    decoration: const InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      hintText: 'أسم الوحدة',
-                                      contentPadding: EdgeInsets.symmetric(
-                                        horizontal: 8.0,
-                                        vertical: 4.0,
-                                      ),
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        drawer: Drawer(
+          child: Container(
+            color: const Color(0xFFFEFEFE),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Divider(color: Colors.deepPurple),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    String unitTitle = '';
+                    int unitPerPiece = 0;
+                    showDialog(
+                      context: context,
+                      builder: (s) => Dialog(
+                        child: Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: Container(
+                            height: 350,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0,
+                              vertical: 16.0,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'أسم الوحدة',
+                                    contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 8.0,
+                                      vertical: 4.0,
                                     ),
-                                    onChanged: (value) => unitTitle = value,
                                   ),
-                                  TextFormField(
-                                    keyboardType: TextInputType.number,
-                                    decoration: const InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      hintText: 'العدد بالحبة',
-                                      contentPadding: EdgeInsets.symmetric(
-                                        horizontal: 8.0,
-                                        vertical: 4.0,
-                                      ),
+                                  onChanged: (value) => unitTitle = value,
+                                ),
+                                TextFormField(
+                                  keyboardType: TextInputType.number,
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'العدد بالحبة',
+                                    contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 8.0,
+                                      vertical: 4.0,
                                     ),
-                                    onChanged: (value) =>
-                                        unitPerPiece = int.parse(value),
                                   ),
-                                  ElevatedButton(
-                                    onPressed: () => Provider.of<UnitProvider>(
-                                            context,
-                                            listen: false)
-                                        .addUnit(unitTitle, unitPerPiece)
-                                        .then(
-                                          (value) =>
-                                              {Navigator.of(context).pop()},
-                                        ),
-                                    child: const Text('إضافة وحدة جديد'),
-                                  ),
-                                ],
-                              ),
+                                  onChanged: (value) =>
+                                      unitPerPiece = int.parse(value),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => Provider.of<UnitProvider>(
+                                          context,
+                                          listen: false)
+                                      .addUnit(unitTitle, unitPerPiece)
+                                      .then(
+                                        (value) =>
+                                            {Navigator.of(context).pop()},
+                                      ),
+                                  child: const Text('إضافة وحدة جديد'),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                      );
-                    },
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(EdgeInsets.zero),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    ),
-                    child: const Text(
-                      'إضافة وحدة جديدة',
-                      style: TextStyle(
-                        color: Colors.deepPurple,
                       ),
+                    );
+                  },
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.zero),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: const Text(
+                    'إضافة وحدة جديدة',
+                    style: TextStyle(
+                      color: Colors.deepPurple,
                     ),
                   ),
-                  const Divider(color: Colors.deepPurple),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'تصدير إلى csv(SOON)',
-                      style: TextStyle(
-                        color: Colors.deepPurple,
-                      ),
+                ),
+                const Divider(color: Colors.deepPurple),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'تصدير إلى csv(SOON)',
+                    style: TextStyle(
+                      color: Colors.deepPurple,
                     ),
                   ),
-                  const Divider(color: Colors.deepPurple),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'إستيراد إلى csv(SOON)',
-                      style: TextStyle(
-                        color: Colors.deepPurple,
-                      ),
+                ),
+                const Divider(color: Colors.deepPurple),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'إستيراد إلى csv(SOON)',
+                    style: TextStyle(
+                      color: Colors.deepPurple,
                     ),
                   ),
-                  const Divider(color: Colors.deepPurple),
-                  TextButton(
-                    onPressed: () {
-                      SQLHelper.deleteDB();
-                    },
-                    child: const Text(
-                      'حذف قاعدة البيانات',
-                      style: TextStyle(
-                        color: Colors.deepPurple,
-                      ),
+                ),
+                const Divider(color: Colors.deepPurple),
+                TextButton(
+                  onPressed: () {
+                    SQLHelper.deleteDB();
+                  },
+                  child: const Text(
+                    'حذف قاعدة البيانات',
+                    style: TextStyle(
+                      color: Colors.deepPurple,
                     ),
                   ),
-                  const Divider(color: Colors.deepPurple),
-                ],
-              ),
+                ),
+                const Divider(color: Colors.deepPurple),
+              ],
             ),
           ),
-          appBar: AppBar(
-            title: const Text(
-              'معملي',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+        ),
+        appBar: AppBar(
+          title: const Text(
+            'معملي',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
             ),
-            centerTitle: true,
           ),
-          floatingActionButton: floatingActionButton[indicator],
-          body: screens[indicator],
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: indicator,
-            onTap: (value) => setIndicator(value),
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.warehouse),
-                label: 'المخزن',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.receipt_long),
-                label: 'العمليات',
-              ),
-            ],
-          ),
+          centerTitle: true,
+        ),
+        floatingActionButton: floatingActionButton[indicator],
+        body: screens[indicator],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: indicator,
+          onTap: (value) => setIndicator(value),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.warehouse),
+              label: 'المخزن',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long),
+              label: 'العمليات',
+            ),
+          ],
         ),
       ),
     );
