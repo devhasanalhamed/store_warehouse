6f140c460185cac778aa317c79f24228e1ad9a55
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-03 23:14:10 +0300

    Update transaction.dart

--- lib/transactions/model/transaction.dart
+++ lib/transactions/model/transaction.dart
@@ -1,17 +1,17 @@
-class TransAction {
+class Transaction {
   final int id;
   final int quantity;
   final int productId;
   final DateTime createdAt;
 
-  TransAction({
+  Transaction({
     required this.id,
     required this.quantity,
     required this.productId,
     required this.createdAt,
   });
 
-  factory TransAction.fromSQL(Map<String, dynamic> db) => TransAction(
+  factory Transaction.fromSQL(Map<String, dynamic> db) => Transaction(
         id: db['id'],
         quantity: db['quantity'],
         productId: db['productId'],
