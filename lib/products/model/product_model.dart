f4d6f754558b9b433dbae9e59e52eeabba894ea3
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-05 06:41:58 +0300

    Update product_model.dart

--- lib/products/model/product_model.dart
+++ lib/products/model/product_model.dart
@@ -1,11 +1,32 @@
+class ProductModel {
+  final int id;
+  final String title;
+  final String description;
+  final String imagePath;
+  final int unitId;
+  final String unitTitle;
+  final int quantity;
+  final int totalAmount;
 
-// class ProductModel extends Product {
-//   ProductModel({
-//     required super.id,
-//     required super.title,
-//     required super.description,
-//     required super.unitId,
-//     required super.quantity,
-//     required super.totalAmount,
-//   });
-// }
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.imagePath,
+    required this.description,
+    required this.unitId,
+    required this.unitTitle,
+    required this.quantity,
+    required this.totalAmount,
+  });
+
+  factory ProductModel.fromSQL(Map<String, dynamic> record) => ProductModel(
+        id: record['id'],
+        title: record['title'],
+        description: record['description'],
+        imagePath: record['imagePath'],
+        unitId: record['unitId'] as int,
+        unitTitle: record['unitTitle'],
+        quantity: record['quantity'] as int,
+        totalAmount: record['totalAmount'] as int,
+      );
+}
