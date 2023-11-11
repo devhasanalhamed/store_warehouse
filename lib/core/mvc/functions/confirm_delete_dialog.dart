0323d1b9aff3d5b0bbdc01f32da52fa119adfb49
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-12 00:43:37 +0300

    Create confirm_delete_dialog.dart

---
+++ lib/core/mvc/functions/confirm_delete_dialog.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_warehouse/core/mvc/view/widgets/elevated_button_component.dart';
+import 'package:store_warehouse/products/controller/product_controller.dart';
+import 'package:store_warehouse/products/model/product_model.dart';
+
+Future<dynamic> confirmDeleteDialog(
+    BuildContext context, ProductModel product) {
+  return showDialog(
+    context: context,
+    builder: (context) => Directionality(
+      textDirection: TextDirection.rtl,
+      child: AlertDialog(
+        title: const Text('هل أنت متأكد؟'),
+        content: const Text('سيتم حذف جميع العمليات المتعلقة بالمنتج'),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        actions: [
+          ElevatedButtonComponent(
+              onPressed: () => Navigator.pop(context), title: 'إلغاء'),
+          TextButton(
+            onPressed: () =>
+                Provider.of<ProductController>(context, listen: false)
+                    .deleteProduct(product)
+                    .then((value) => Navigator.pop(context)),
+            child: const Text('حذف'),
+          ),
+        ],
+      ),
+    ),
+  );
+}
