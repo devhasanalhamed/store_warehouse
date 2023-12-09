fb69fe5978ae7f0d28dfc30ed0d3ffa26f06b7d7
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-09 22:18:36 +0300

    Update products_screen.dart

--- lib/products/view/screen/products_screen.dart
+++ lib/products/view/screen/products_screen.dart
@@ -17,43 +17,6 @@ class ProductsScreen extends StatefulWidget {
 class ProductsScreenState extends State<ProductsScreen> {
   @override
   Widget build(BuildContext context) {
-    Provider.of<TransactionController>(context);
-    return FutureBuilder<List<ProductModel>>(
-      future: Provider.of<ProductController>(context).getProduct(),
-      builder: (context, snapshot) {
-        log('build: future product screen has built');
-        if (snapshot.hasData) {
-          if (snapshot.data!.isNotEmpty) {
-            return Column(
-              children: [
-                const SizedBox(height: 16.0),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) =>
-                        ProductWidget(product: snapshot.data![index]),
-                  ),
-                ),
-              ],
-            );
-          } else {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/no_products.png',
-                  width: 300,
-                ),
-                const Center(
-                  child: Text('لا توجد منتجات بعد'),
-                ),
-              ],
-            );
-          }
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    );
+    return Text('');
   }
 }
