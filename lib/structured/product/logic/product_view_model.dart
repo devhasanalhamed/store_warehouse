fc6e318a4a6127cc54f9f8d33d8cde0817776188
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-10 17:11:50 +0300

    Update product_view_model.dart

--- lib/structured/product/logic/product_view_model.dart
+++ lib/structured/product/logic/product_view_model.dart
@@ -1,3 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:store_warehouse/core/utils/database/db_config.dart';
+import 'package:store_warehouse/structured/product/data/product_model.dart';
 
-class ProductViewModel extends ChangeNotifier {}
+class ProductViewModel extends ChangeNotifier {
+  List<ProductModel> productList = [];
+  Future<List<ProductModel>> fetchProducts() async {
+    print('here');
+    final x = await DbConfig.getProducts();
+    productList = x;
+    print(x);
+    return x;
+  }
+
+  Future<List<Map<String, dynamic>>>
+      fetchProductsWithQuantitiesAndUnitTitles() async {
+    final db = await DbConfig.getInstance();
+    final x = await db.rawQuery('''
+    SELECT
+        p.id AS product_id,
+        p.name AS product_name,
+        u.name AS unit_title,
+        COALESCE(SUM(t.amount), 0) AS quantity
+    FROM
+        product p
+    JOIN
+        unit u ON p.unit_id = u.id
+    LEFT JOIN
+        transactions t ON p.id = t.product_id
+    GROUP BY
+        p.id, p.name, u.name;
+  ''');
+    print(x);
+    return x;
+  }
+}
