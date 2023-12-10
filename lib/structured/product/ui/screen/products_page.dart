4c6084547da3ee0ee0efa9794b6ba195ad06b9b9
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-10 17:11:53 +0300

    Update products_page.dart

--- lib/structured/product/ui/screen/products_page.dart
+++ lib/structured/product/ui/screen/products_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_warehouse/structured/product/logic/product_view_model.dart';
 import 'package:store_warehouse/structured/product/ui/widget/product_widget.dart';
 
 class ProductsPage extends StatelessWidget {
@@ -6,16 +8,10 @@ class ProductsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ProductWidget(),
-        ProductWidget(),
-        ProductWidget(),
-        ProductWidget(),
-        ProductWidget(),
-        ProductWidget(),
-        ProductWidget(),
-      ],
+    return ListView.builder(
+      itemCount: context.read<ProductViewModel>().productList.length,
+      itemBuilder: (context, index) => ProductWidget(
+          product: context.read<ProductViewModel>().productList[index]),
     );
   }
 }
