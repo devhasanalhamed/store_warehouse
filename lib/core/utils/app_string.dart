88e751c9c97a6d18c7b3ba40b29b5cf4003d2460
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-10 13:12:37 +0300

    Create app_string.dart

---
+++ lib/core/utils/app_string.dart
@@ -0,0 +1,7 @@
+class AppString {
+  static const home = 'الرئيسية';
+  static const transactions = 'العمليات';
+  static const products = 'المنتجات';
+  static const reports = 'التقارير';
+  static const settings = 'الاعدادات';
+}
