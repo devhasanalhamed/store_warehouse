11c80577a0f9e982be0c0dbc433d64c3115e2050
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-11 07:32:10 +0300

    Create products_page.dart

---
+++ lib/product/ui/screen/products_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_warehouse/product/logic/product_view_model.dart';
+import 'package:store_warehouse/product/ui/widget/product_widget.dart';
+
+class ProductsPage extends StatelessWidget {
+  const ProductsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: context.read<ProductViewModel>().productList.length,
+      itemBuilder: (context, index) => ProductWidget(
+          product: context.read<ProductViewModel>().productList[index]),
+    );
+  }
+}
