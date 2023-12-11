5a559dde1180f754995551c021b15be2f3605378
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-11 07:32:59 +0300

    Create products_screen.dart

---
+++ lib/get_rid/products/view/screen/products_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductsScreen extends StatefulWidget {
+  const ProductsScreen({Key? key}) : super(key: key);
+
+  @override
+  ProductsScreenState createState() => ProductsScreenState();
+}
+
+class ProductsScreenState extends State<ProductsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('');
+  }
+}
