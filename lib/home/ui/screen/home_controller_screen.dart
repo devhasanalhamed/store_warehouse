70610fa4db12e5ba5875a98ef3e7886fd4a889e6
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-11 13:18:40 +0300

    Update home_controller_screen.dart

--- lib/home/ui/screen/home_controller_screen.dart
+++ lib/home/ui/screen/home_controller_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/core/utils/app_string.dart';
 import 'package:store_warehouse/home/logic/home_view_model.dart';
+import 'package:store_warehouse/home/ui/screen/settings_page.dart';
 import 'package:store_warehouse/home/ui/widget/custom_bottom_navigation_bar.dart';
 import 'package:store_warehouse/product/ui/screen/add_product_screen.dart';
 import 'package:store_warehouse/product/ui/screen/products_page.dart';
@@ -11,7 +12,7 @@ class HomeControllerScreen extends StatelessWidget {
   const HomeControllerScreen({Key? key}) : super(key: key);
 
   final pages = const [
-    Center(child: Text('screen')),
+    SettingsPage(),
     Center(child: Text('reports')),
     Center(child: Text('home')),
     TransactionsPage(),
