bc9099d22bb8d11ebfeed94437a811768dc8c6c0
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-12 13:14:53 +0300

    Update add_product_screen.dart

--- lib/product/ui/screen/add_product_screen.dart
+++ lib/product/ui/screen/add_product_screen.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/product/data/product_model.dart';
 import 'package:store_warehouse/product/logic/product_view_model.dart';
+import 'package:store_warehouse/shared/widget/text_form_field_component.dart';
+import 'package:store_warehouse/shared/widget/upload_image.dart';
+import 'package:store_warehouse/unit/ui/widget/select_unit_component.dart';
 
 class AddProductScreen extends StatelessWidget {
   AddProductScreen({Key? key}) : super(key: key);
 
-  final GlobalKey<FormState> _formKey = GlobalKey();
+  final GlobalKey<FormState> formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
+    late String title;
+    late String description;
+    late String notes;
+    String? imagePath;
+    late int unitId;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('إضافة منتج'),
@@ -17,17 +27,19 @@ class AddProductScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Form(
-          key: context.read<ProductViewModel>().formKey,
+          key: formKey,
           child: ListView(
             children: [
               const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'عنوان',
-                  border: OutlineInputBorder(),
-                ),
-                onSaved: (newValue) =>
-                    context.read<ProductViewModel>().title = newValue!,
+              UploadImage(
+                onTakePhoto: (path) {
+                  return imagePath = path!;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormFieldComponent(
+                hintText: 'عنوان',
+                onSaved: (newValue) => title = newValue!,
                 validator: (value) {
                   if (value!.isNotEmpty) {
                     return null;
@@ -36,13 +48,9 @@ class AddProductScreen extends StatelessWidget {
                 },
               ),
               const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'وصف',
-                  border: OutlineInputBorder(),
-                ),
-                onSaved: (newValue) =>
-                    context.read<ProductViewModel>().description = newValue!,
+              TextFormFieldComponent(
+                hintText: 'وصف',
+                onSaved: (newValue) => description = newValue!,
                 validator: (value) {
                   if (value!.isNotEmpty) {
                     return null;
@@ -51,13 +59,9 @@ class AddProductScreen extends StatelessWidget {
                 },
               ),
               const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'الملاحظات',
-                  border: OutlineInputBorder(),
-                ),
-                onSaved: (newValue) =>
-                    context.read<ProductViewModel>().notes = newValue!,
+              TextFormFieldComponent(
+                hintText: 'الملاحظات',
+                onSaved: (newValue) => notes = newValue!,
                 validator: (value) {
                   if (value!.isNotEmpty) {
                     return null;
@@ -66,42 +70,27 @@ class AddProductScreen extends StatelessWidget {
                 },
               ),
               const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'عنوان الصورة',
-                  border: OutlineInputBorder(),
-                ),
-                onSaved: (newValue) =>
-                    context.read<ProductViewModel>().imagePath = newValue!,
-                validator: (value) {
-                  if (value!.isNotEmpty) {
-                    return null;
-                  }
-                  return 'f';
-                },
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'الوحدة',
-                  border: OutlineInputBorder(),
-                ),
-                onSaved: (newValue) =>
-                    context.read<ProductViewModel>().unitId = newValue!,
-                validator: (value) {
-                  if (value!.isNotEmpty) {
-                    return null;
-                  }
-                  return 'f';
-                },
+              SelectUnitComponent(
+                onChanged: (value) => unitId = value,
               ),
               const SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: () {
-                  if (true) {
-                    print('valid');
-
-                    context.read<ProductViewModel>().x();
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    context
+                        .read<ProductViewModel>()
+                        .addProduct(
+                          ProductModel(
+                            id: 0,
+                            title: title,
+                            imagePath: imagePath ?? '',
+                            description: description,
+                            unitId: unitId,
+                            notes: notes,
+                          ),
+                        )
+                        .then((value) => Navigator.pop(context));
                   }
                 },
                 child: const Text('Add'),
