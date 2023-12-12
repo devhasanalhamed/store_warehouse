96c7add538faf1d84c47ed9f745edb36f22b2692
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-12 13:15:05 +0300

    Create text_form_field_component.dart

---
+++ lib/shared/widget/text_form_field_component.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:store_warehouse/core/utils/app_design.dart';
+
+class TextFormFieldComponent extends StatelessWidget {
+  final String? hintText;
+  final FormFieldSetter<String>? onSaved;
+  final FormFieldValidator<String>? validator;
+  const TextFormFieldComponent({
+    Key? key,
+    this.hintText,
+    this.onSaved,
+    this.validator,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(AppDesign.circularRadius),
+        ),
+        contentPadding: const EdgeInsets.all(AppDesign.largePadding),
+      ),
+      onSaved: onSaved,
+      validator: validator,
+    );
+  }
+}
