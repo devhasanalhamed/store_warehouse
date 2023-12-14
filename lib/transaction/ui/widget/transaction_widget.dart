d1f3e8a0a437e8e1fd2c42221ed8c041832d3950
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-14 18:16:50 +0300

    Update transaction_widget.dart

--- lib/transaction/ui/widget/transaction_widget.dart
+++ lib/transaction/ui/widget/transaction_widget.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/product/ui/widget/show_product.component.dart';
+import 'package:store_warehouse/shared/widget/text_form_field_component.dart';
 import 'package:store_warehouse/transaction/data/transaction_model.dart';
 import 'package:store_warehouse/transaction/logic/transaction_view_model.dart';
 import 'package:store_warehouse/transaction_type/data/transaction_type_model.dart';
@@ -59,12 +60,68 @@ class TransactionWidget extends StatelessWidget {
           if (direction == DismissDirection.startToEnd) {
             // Edit
             // Implement your edit confirmation logic here
-            return true;
+            final bool shouldDelete = await showDeleteConfirmationDialog();
+            return shouldDelete;
           } else if (direction == DismissDirection.endToStart) {
             // Delete
             // Implement your delete confirmation logic here
-            final bool shouldDelete = await showDeleteConfirmationDialog();
-            return shouldDelete;
+            await showDialog(
+              context: context,
+              builder: (context) {
+                int amount = transaction.amount;
+                String notes = transaction.notes;
+                return Dialog(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormFieldComponent(
+                          initialValue: transaction.amount.toString(),
+                          keyboardType: TextInputType.number,
+                          labelText: 'الكمية',
+                          onChanged: (value) => amount = int.parse(value),
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormFieldComponent(
+                          initialValue: transaction.notes,
+                          keyboardType: TextInputType.number,
+                          labelText: 'الملاحظات',
+                          onChanged: (value) => notes = value,
+                        ),
+                        const SizedBox(height: 16.0),
+                        Row(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () => context
+                                  .read<TransactionViewModel>()
+                                  .updateTransaction(TransactionModel(
+                                    transactionId: transaction.transactionId,
+                                    productId: transaction.productId,
+                                    transactionTypeId:
+                                        transaction.transactionTypeId,
+                                    amount: amount,
+                                    notes: notes,
+                                    createdAt: DateTime.now(),
+                                  ))
+                                  .then((value) => Navigator.pop(context)),
+                              child: const Text('تعديل'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text('data'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+            return false;
           }
           return false;
         },
