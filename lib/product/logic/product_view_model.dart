77cee349f34e6c8a5d39f5fb78fbb307f05446f3
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-14 15:49:31 +0300

    Update product_view_model.dart

--- lib/product/logic/product_view_model.dart
+++ lib/product/logic/product_view_model.dart
@@ -5,35 +5,20 @@ import 'package:store_warehouse/product/data/product_model.dart';
 class ProductViewModel extends ChangeNotifier {
   final db = ProductDAO();
   List<ProductModel> productList = [];
+
   Future<void> getProducts() async {
     final x = await db.fetchProducts();
     productList = x;
     notifyListeners();
   }
 
-  // Future<List<Map<String, dynamic>>>
-  //     fetchProductsWithQuantitiesAndUnitTitles() async {
-  //   final db = await DbConfig.getInstance();
-  //   final x = await db.rawQuery('''
-  //   SELECT
-  //       p.id AS product_id,
-  //       p.name AS product_name,
-  //       u.name AS unit_title,
-  //       COALESCE(SUM(t.amount), 0) AS quantity
-  //   FROM
-  //       product p
-  //   JOIN
-  //       unit u ON p.unit_id = u.id
-  //   LEFT JOIN
-  //       transactions t ON p.id = t.product_id
-  //   GROUP BY
-  //       p.id, p.name, u.name;
-  // ''');
-  //   return x;
-  // }
-
   Future<void> addProduct(ProductModel product) async {
     await db.insert(product);
+    getProducts();
+  }
+
+  Future<void> updateProduct(ProductModel product) async {
+    await db.update(product);
     getProducts();
   }
 
