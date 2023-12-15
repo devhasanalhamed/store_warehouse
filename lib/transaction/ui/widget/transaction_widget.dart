ad0276c8debf71518526cde60f3e7f1ec9e689b0
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-15 13:59:46 +0300

    Update transaction_widget.dart

--- lib/transaction/ui/widget/transaction_widget.dart
+++ lib/transaction/ui/widget/transaction_widget.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/product/ui/widget/show_product.component.dart';
-import 'package:store_warehouse/shared/widget/text_form_field_component.dart';
+import 'package:store_warehouse/shared/function/dialog/delete_confirm.dart';
+import 'package:store_warehouse/shared/function/dialog/edit_transaction_dialog.dart';
 import 'package:store_warehouse/transaction/data/transaction_model.dart';
 import 'package:store_warehouse/transaction/logic/transaction_view_model.dart';
 import 'package:store_warehouse/transaction_type/data/transaction_type_model.dart';
@@ -16,28 +17,6 @@ class TransactionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future<bool> showDeleteConfirmationDialog() async {
-      return await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text("Delete Item"),
-            content: Text("Are you sure you want to delete this item?"),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(false),
-                child: Text("Cancel"),
-              ),
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(true),
-                child: Text("Delete"),
-              ),
-            ],
-          );
-        },
-      );
-    }
-
     final TransactionTypeModel transactionType = context
         .read<TransactionTypeViewModel>()
         .getTransactionById(transaction.transactionTypeId);
@@ -48,82 +27,18 @@ class TransactionWidget extends StatelessWidget {
       child: Dismissible(
         key: UniqueKey(),
         onDismissed: (direction) {
-          if (direction == DismissDirection.endToStart) {
-            // Edit
-            // Implement your edit logic here
-          } else if (direction == DismissDirection.startToEnd) {
-            // Delete
-            // Implement your delete logic here
-          }
+          context
+              .read<TransactionViewModel>()
+              .deleteTransaction(transaction.transactionId);
         },
         confirmDismiss: (direction) async {
           if (direction == DismissDirection.startToEnd) {
-            // Edit
-            // Implement your edit confirmation logic here
-            final bool shouldDelete = await showDeleteConfirmationDialog();
+            final bool shouldDelete = await deleteConfirmDialog(context);
             return shouldDelete;
-          } else if (direction == DismissDirection.endToStart) {
-            // Delete
-            // Implement your delete confirmation logic here
-            await showDialog(
-              context: context,
-              builder: (context) {
-                int amount = transaction.amount;
-                String notes = transaction.notes;
-                return Dialog(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16.0, horizontal: 8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextFormFieldComponent(
-                          initialValue: transaction.amount.toString(),
-                          keyboardType: TextInputType.number,
-                          labelText: 'الكمية',
-                          onChanged: (value) => amount = int.parse(value),
-                        ),
-                        const SizedBox(height: 16.0),
-                        TextFormFieldComponent(
-                          initialValue: transaction.notes,
-                          keyboardType: TextInputType.number,
-                          labelText: 'الملاحظات',
-                          onChanged: (value) => notes = value,
-                        ),
-                        const SizedBox(height: 16.0),
-                        Row(
-                          children: [
-                            ElevatedButton(
-                              onPressed: () => context
-                                  .read<TransactionViewModel>()
-                                  .updateTransaction(TransactionModel(
-                                    transactionId: transaction.transactionId,
-                                    productId: transaction.productId,
-                                    transactionTypeId:
-                                        transaction.transactionTypeId,
-                                    amount: amount,
-                                    notes: notes,
-                                    createdAt: DateTime.now(),
-                                  ))
-                                  .then((value) => Navigator.pop(context)),
-                              child: const Text('تعديل'),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Text('data'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
+          } else {
+            await editTransactionDialog(context, transaction);
             return false;
           }
-          return false;
         },
         background: Container(
           color: Colors.red,
