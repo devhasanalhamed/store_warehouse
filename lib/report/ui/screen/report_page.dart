e4d80ec9d2b53cd4433fb0dd1b29567c0a3d016b
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-17 17:08:40 +0300

    Update report_page.dart

--- lib/report/ui/screen/report_page.dart
+++ lib/report/ui/screen/report_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:store_warehouse/core/utils/app_design.dart';
+import 'package:store_warehouse/product/logic/product_view_model.dart';
 import 'package:store_warehouse/report/logic/report_view_model.dart';
 
 class ReportPage extends StatelessWidget {
@@ -13,10 +14,17 @@ class ReportPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productList =
+        Provider.of<ProductViewModel>(context, listen: false).productList;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: AppDesign.smallPadding),
+      padding: const EdgeInsets.symmetric(
+        horizontal: AppDesign.smallPadding,
+        vertical: AppDesign.smallPadding,
+      ),
       child: Consumer<ReportViewModel>(
-        builder: (_, state, __) => ListView(
+        builder: (_, state, __) => Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const Text('اليوم'),
             const SizedBox(height: 8.0),
@@ -36,68 +44,99 @@ class ReportPage extends StatelessWidget {
                         ],
                       ),
                       for (var transaction in state.todayReport)
-                        TableRow(children: [
-                          Text(transaction.productId.toString()),
-                          Text(transaction.transactionTypeId.toString()),
-                          Text(transaction.amount.toString()),
-                          Text(transaction.createdAt.toString()),
-                          Text(transaction.notes.toString()),
-                        ]),
+                        TableRow(
+                          children: [
+                            Text(productList
+                                .firstWhere((element) =>
+                                    element.id == transaction.productId)
+                                .title),
+                            Text(transaction.transactionTypeId == 1
+                                ? 'إضافة'
+                                : 'سحب'),
+                            Text(transaction.amount.toString()),
+                            Text(transaction.createdAt.toIso8601String()),
+                            Text(transaction.notes.toString()),
+                          ],
+                        ),
                     ],
                   ),
                 ],
               ),
             ),
             ElevatedButton(
-                onPressed: () async {
-                  try {
-                    // Create an Excel workbook
-                    final excel = Excel.createExcel();
+              onPressed: () async {
+                try {
+                  // Create an Excel workbook
+                  final excel = Excel.createExcel();
 
-                    // Add a sheet to the workbook
-                    final sheet = excel['Sheet1'];
+                  // Add a sheet to the workbook
+                  final sheet = excel['Sheet1'];
 
-                    // Add headers to the sheet
+                  // Add headers to the sheet
+                  sheet.appendRow([
+                    const TextCellValue('رقم العملية'),
+                    const TextCellValue('المنتج'),
+                    const TextCellValue('نوع العملية'),
+                    const TextCellValue('الكمية'),
+                    const TextCellValue('الملاحظات'),
+                    const TextCellValue('تاريخ العملية')
+                  ]);
+
+                  // Add transaction data to the sheet
+                  for (var transaction in state.todayReport) {
                     sheet.appendRow([
-                      const TextCellValue('Transaction ID'),
-                      const TextCellValue('Product ID'),
-                      const TextCellValue('Transaction Type'),
-                      const TextCellValue('Amount'),
-                      const TextCellValue('Notes'),
-                      const TextCellValue('Created At')
+                      TextCellValue(transaction.transactionId.toString()),
+                      TextCellValue(productList
+                          .firstWhere(
+                              (element) => element.id == transaction.productId)
+                          .title),
+                      TextCellValue(
+                          transaction.transactionTypeId == 1 ? 'إضافة' : 'سحب'),
+                      TextCellValue(transaction.amount.toString()),
+                      TextCellValue(transaction.notes.toString()),
+                      TextCellValue(transaction.createdAt.toIso8601String()),
                     ]);
-
-                    // Add transaction data to the sheet
-                    for (var transaction in state.todayReport) {
-                      sheet.appendRow([
-                        TextCellValue(transaction.transactionId.toString()),
-                        TextCellValue(transaction.productId.toString()),
-                        TextCellValue(transaction.transactionTypeId.toString()),
-                        TextCellValue(transaction.amount.toString()),
-                        TextCellValue(transaction.notes.toString()),
-                        TextCellValue(transaction.createdAt.toIso8601String()),
-                      ]);
-                    }
-
-                    // Get the app's documents directory
-                    final Directory? appDocDir =
-                        await getExternalStorageDirectory();
-
-                    // Define the Excel file path
-                    final String excelPath =
-                        '${appDocDir!.path}/${DateTime.now().microsecond}_report.xlsx';
-
-                    // Save the Excel file
-                    final List<int>? bytes = excel.encode();
-                    File(excelPath)
-                        .writeAsBytesSync(Uint8List.fromList(bytes!));
-                    print('Excel file saved at: $excelPath');
-                  } catch (e) {
-                    // Handle exceptions (e.g., log, show an error message, etc.)
-                    print('Error exporting to Excel: $e');
                   }
-                },
-                child: const Text('تصدير')),
+
+                  // Get the app's documents directory
+                  final Directory? appDocDir =
+                      await getExternalStorageDirectory();
+
+                  // Define the Excel file path
+                  final String excelPath = '${appDocDir!.path}/report.xlsx';
+
+                  // Save the Excel file
+                  final List<int>? bytes = excel.encode();
+                  File(excelPath).writeAsBytesSync(Uint8List.fromList(bytes!));
+                  print('Excel file saved at: $excelPath');
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('تم تصدير ملف'),
+                    ),
+                  );
+                } catch (e) {
+                  // Handle exceptions (e.g., log, show an error message, etc.)
+                  print('Error exporting to Excel: $e');
+                }
+              },
+              child: const Text('تصدير'),
+            ),
+            ToggleButtons(
+              onPressed: (index) {
+                print(index);
+              },
+              isSelected: [true, false, false, false],
+              constraints: BoxConstraints(
+                minHeight: 40,
+                minWidth: 90,
+              ),
+              children: [
+                Text('اليوم'),
+                Text('الأسبوع'),
+                Text('الشهر'),
+                Text('مخصص'),
+              ],
+            ),
           ],
         ),
       ),
