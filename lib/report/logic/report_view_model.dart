46697152653cda5663c46920e327ff6b553bdd99
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-17 10:36:06 +0300

    Create report_view_model.dart

---
+++ lib/report/logic/report_view_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:store_warehouse/core/database/dao/transaction_dao.dart';
+import 'package:store_warehouse/transaction/data/transaction_model.dart';
+
+class ReportViewModel extends ChangeNotifier {
+  final TransactionDAO transactionDAO = TransactionDAO();
+  List<TransactionModel> todayReport = [];
+  List<TransactionModel> weekReport = [];
+  List<TransactionModel> monthReport = [];
+  List<TransactionModel> customReport = [];
+
+  Future<void> getTodayReport() async {
+    final DateTime now = DateTime.now();
+    final startOfToday = DateTime(now.year, now.month, now.day, 0, 0, 0);
+    final endOfToday = DateTime(now.year, now.month, now.day, 23, 59, 59);
+    final result = await transactionDAO.report(startOfToday, endOfToday);
+    todayReport = result;
+    notifyListeners();
+  }
+}
