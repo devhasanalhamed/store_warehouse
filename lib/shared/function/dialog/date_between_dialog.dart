93e400b9184372d81681574ebe33d62e84e02946
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-20 12:19:53 +0300

    Create date_between_dialog.dart

---
+++ lib/shared/function/dialog/date_between_dialog.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_warehouse/core/utils/app_design.dart';
+import 'package:store_warehouse/report/logic/report_view_model.dart';
+
+dateBetweenDialog(BuildContext context) => showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController startDateController = TextEditingController();
+        TextEditingController endDateController = TextEditingController();
+        late DateTime startDate;
+        late DateTime endDate;
+        return Dialog(
+          child: Container(
+            padding: const EdgeInsets.only(
+              left: AppDesign.smallPadding,
+              top: 16.0,
+              right: AppDesign.smallPadding,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                AppDesign.circularRadius,
+              ),
+            ),
+            width: 300,
+            height: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                TextFormField(
+                    controller: startDateController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text('التاريخ من:'),
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      final value = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(DateTime.now().year - 4),
+                        lastDate: DateTime(DateTime.now().year + 4),
+                      );
+                      if (value == null) return;
+                      startDate = value;
+                      startDateController.text =
+                          '${value.day}-${value.month}-${value.year}';
+                    }),
+                const SizedBox(height: AppDesign.largePadding),
+                TextFormField(
+                    controller: endDateController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text('التاريخ إلى:'),
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      final value = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(DateTime.now().year - 4),
+                        lastDate: DateTime(DateTime.now().year + 4),
+                      );
+                      if (value == null) return;
+                      endDate = value;
+                      endDateController.text =
+                          '${value.day}-${value.month}-${value.year}';
+                    }),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<ReportViewModel>().updateReportType(3);
+                    context
+                        .read<ReportViewModel>()
+                        .getCustomReport(startDate, endDate);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('موافق'),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
