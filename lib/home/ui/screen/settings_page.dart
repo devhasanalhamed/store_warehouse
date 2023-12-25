9ddc496d63b4b8ba9153d59ada04e5acb5bbfb23
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-25 12:39:54 +0300

    Update settings_page.dart

--- lib/home/ui/screen/settings_page.dart
+++ lib/home/ui/screen/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store_warehouse/core/database/db_config.dart';
+import 'package:store_warehouse/core/database/sql_helper.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -12,6 +13,24 @@ class SettingsPage extends StatelessWidget {
           onTap: () => DbConfig.delete(),
           title: const Text('delete database'),
         ),
+        ListTile(
+          onTap: () => SQLHelper().backupDB().then((value) =>
+              ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(
+                    const SnackBar(content: Text('تم نسخ قاعدة البيانات')))),
+          title: const Text('نسخ احتياطي'),
+        ),
+        const Divider(),
+        ListTile(
+          onTap: () => SQLHelper().restoreDB().then((value) =>
+              ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(
+                    const SnackBar(content: Text('أعد تشغيل التطبيق')))),
+          title: const Text('استيراد قاعدة بيانات'),
+        ),
+        const Divider(),
       ],
     );
   }
