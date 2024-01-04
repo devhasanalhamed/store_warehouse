18c9268d3c0057830852dbfaea296f6c5dd35a01
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-01-04 13:19:22 +0300

    Update home_view_model.dart

--- lib/home/logic/home_view_model.dart
+++ lib/home/logic/home_view_model.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeViewModel extends ChangeNotifier {
-  static const startIndex = 0;
+  static const startIndex = 2;
   int currentIndex = startIndex;
   void updateCurrentIndex(int index) {
     currentIndex = index;
