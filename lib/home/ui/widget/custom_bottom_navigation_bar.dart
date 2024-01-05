29bc04847907bc3d6b66662b305df9689d7d9fe2
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-01-05 12:32:59 +0300

    Update custom_bottom_navigation_bar.dart

--- lib/home/ui/widget/custom_bottom_navigation_bar.dart
+++ lib/home/ui/widget/custom_bottom_navigation_bar.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:store_warehouse/core/utils/app_string.dart';
+import 'package:store_warehouse/core/constants/app_string.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
