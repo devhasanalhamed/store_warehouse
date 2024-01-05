e53a3f670239a730013add675b4ee36da343aa63
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-01-05 12:33:34 +0300

    Delete app_string.dart

--- lib/core/utils/app_string.dart
+++
@@ -1,7 +0,0 @@
-class AppString {
-  static const home = 'الرئيسية';
-  static const transactions = 'العمليات';
-  static const products = 'المنتجات';
-  static const reports = 'التقارير';
-  static const settings = 'الاعدادات';
-}
