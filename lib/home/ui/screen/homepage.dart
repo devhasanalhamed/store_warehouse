f3aae2db82db0b2ebda7bf6ce1e41bd7a9238a01
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-01-08 13:05:16 +0300

    Update homepage.dart

--- lib/home/ui/screen/homepage.dart
+++ lib/home/ui/screen/homepage.dart
@@ -21,246 +21,207 @@ class Homepage extends StatelessWidget {
         horizontal: AppDesign.mediumPadding,
         vertical: AppDesign.smallPadding,
       ),
-      child: Stack(
+      child: ListView(
         children: [
-          SizedBox(
+          Container(
+            height: 190,
             width: double.infinity,
-            height: double.infinity,
-            child: ImageFiltered(
-              imageFilter: ImageFilter.blur(sigmaY: 100, sigmaX: 100),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 100,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ],
-                  ),
-                ],
+            padding: const EdgeInsets.all(AppDesign.smallPadding),
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardTheme.color,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(
+                  AppDesign.circularRadius,
+                ),
               ),
             ),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${DateTime.now().weekday}'.tr(),
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                      ),
+                      const SizedBox(height: AppDesign.smallPadding),
+                      Text(
+                        DateFormat.yMd().format(DateTime.now()),
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: PieChart(
+                    dataMap: {
+                      "add".tr(): 0,
+                      "consume".tr(): 0,
+                    },
+                    colorList: const [
+                      Color(0xFF81F986),
+                      Color(0xFFE85158),
+                    ],
+                    chartLegendSpacing: 32,
+                    chartRadius: MediaQuery.of(context).size.width / 3.2,
+                    initialAngleInDegree: 90,
+                    chartType: ChartType.disc,
+                    legendOptions: const LegendOptions(
+                      showLegendsInRow: true,
+                      legendPosition: LegendPosition.bottom,
+                      showLegends: true,
+                      legendTextStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    chartValuesOptions: const ChartValuesOptions(
+                      decimalPlaces: 0,
+                    ),
+                    animationDuration: const Duration(milliseconds: 0),
+                  ),
+                )
+              ],
+            ),
           ),
-          ListView(
+          const SizedBox(height: AppDesign.smallPadding),
+          Row(
             children: [
-              Container(
-                height: 190,
-                width: double.infinity,
-                padding: const EdgeInsets.all(AppDesign.smallPadding),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).cardTheme.color,
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(
+              GridElement(
+                title: 'totalProducts',
+                futureFunction: ProductDAO().fetchProductsCount(),
+              ),
+              const SizedBox(width: AppDesign.smallPadding),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color(0xFF4C4C4C).withOpacity(0.1),
+                    ),
+                    borderRadius: BorderRadius.circular(
                       AppDesign.circularRadius,
                     ),
                   ),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            '${DateTime.now().weekday}'.tr(),
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleLarge
-                                ?.copyWith(
-                                  color: Theme.of(context).colorScheme.primary,
-                                ),
-                          ),
-                          const SizedBox(height: AppDesign.smallPadding),
-                          Text(
-                            DateFormat.yMd().format(DateTime.now()),
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                        ],
+                  height: 190,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(
+                        AppDesign.circularRadius,
                       ),
                     ),
-                    Expanded(
-                      flex: 4,
-                      child: PieChart(
-                        dataMap: {
-                          "add".tr(): 0,
-                          "consume".tr(): 0,
-                        },
-                        colorList: const [
-                          Color(0xFF81F986),
-                          Color(0xFFE85158),
-                        ],
-                        chartLegendSpacing: 32,
-                        chartRadius: MediaQuery.of(context).size.width / 3.2,
-                        initialAngleInDegree: 90,
-                        chartType: ChartType.disc,
-                        legendOptions: const LegendOptions(
-                          showLegendsInRow: true,
-                          legendPosition: LegendPosition.bottom,
-                          showLegends: true,
-                          legendTextStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        chartValuesOptions: const ChartValuesOptions(
-                          decimalPlaces: 0,
-                        ),
-                        animationDuration: const Duration(milliseconds: 0),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(height: AppDesign.smallPadding),
-              Row(
-                children: [
-                  GridElement(
-                    title: 'totalProducts',
-                    futureFunction: ProductDAO().fetchProductsCount(),
-                  ),
-                  const SizedBox(width: AppDesign.smallPadding),
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: const Color(0xFF4C4C4C).withOpacity(0.1),
-                        ),
-                        borderRadius: BorderRadius.circular(
-                          AppDesign.circularRadius,
-                        ),
-                      ),
-                      height: 190,
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(
-                            AppDesign.circularRadius,
-                          ),
-                        ),
-                        child: Stack(
-                          alignment: Alignment.bottomCenter,
-                          children: [
-                            FutureBuilder<Map<String, dynamic>>(
-                              future: TransactionDAO().fetchMostUsedProduct(),
-                              builder: (context, snapshot) {
-                                if (snapshot.hasData) {
-                                  return Stack(
-                                    alignment: Alignment.center,
-                                    children: [
-                                      if (File(
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        FutureBuilder<Map<String, dynamic>>(
+                          future: TransactionDAO().fetchMostUsedProduct(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  if (File(
+                                    snapshot.data!['image_path'],
+                                  ).existsSync())
+                                    Image.file(
+                                      File(
                                         snapshot.data!['image_path'],
-                                      ).existsSync())
-                                        Image.file(
-                                          File(
-                                            snapshot.data!['image_path'],
+                                      ),
+                                      height: double.infinity,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  FittedBox(
+                                    child: Center(
+                                      child: Container(
+                                        padding: const EdgeInsets.all(
+                                            AppDesign.smallPadding),
+                                        decoration: const ShapeDecoration(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(
+                                                AppDesign.circularRadius,
+                                              ),
+                                            ),
                                           ),
-                                          height: double.infinity,
-                                          width: double.infinity,
-                                          fit: BoxFit.cover,
+                                          color: Colors.white54,
                                         ),
-                                      FittedBox(
-                                        child: Center(
-                                          child: Container(
-                                            padding: const EdgeInsets.all(
-                                                AppDesign.smallPadding),
-                                            decoration: const ShapeDecoration(
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.all(
-                                                  Radius.circular(
-                                                    AppDesign.circularRadius,
-                                                  ),
-                                                ),
-                                              ),
-                                              color: Colors.white54,
-                                            ),
-                                            child: Text(
-                                              snapshot.data!['name'],
-                                              style: const TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 24.0,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
+                                        child: Text(
+                                          snapshot.data!['name'],
+                                          style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 24.0,
+                                            fontWeight: FontWeight.bold,
                                           ),
                                         ),
                                       ),
-                                    ],
-                                  );
-                                } else {
-                                  return SvgPicture.asset(
-                                    'assets/svg/productPlaceHolder.svg',
-                                    width: double.infinity,
-                                    height: double.infinity,
-                                    fit: BoxFit.cover,
-                                  );
-                                }
-                              },
-                            ),
-                            Container(
-                              width: double.infinity,
-                              height: double.infinity,
-                              padding:
-                                  const EdgeInsets.all(AppDesign.mediumPadding),
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  colors: [
-                                    const Color(0xFF4C4C4C),
-                                    Colors.white.withOpacity(0.1),
-                                  ],
-                                  begin: Alignment.bottomCenter,
-                                  end: Alignment.topCenter,
-                                ),
-                              ),
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Text(
-                                  'mostUsedProduct'.tr(),
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
+                                ],
+                              );
+                            } else {
+                              return SvgPicture.asset(
+                                'assets/svg/productPlaceHolder.svg',
+                                width: double.infinity,
+                                height: double.infinity,
+                                fit: BoxFit.cover,
+                              );
+                            }
+                          },
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          padding:
+                              const EdgeInsets.all(AppDesign.mediumPadding),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                const Color(0xFF4C4C4C),
+                                Colors.white.withOpacity(0.1),
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                            ),
+                          ),
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text(
+                              'mostUsedProduct'.tr(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
                   ),
-                ],
+                ),
               ),
-              const SizedBox(height: AppDesign.smallPadding),
-              Row(
-                children: [
-                  GridElement(
-                    title: 'lastBackup',
-                    futureFunction: SQLHelper().getLastBackup(),
-                    extendedColor: Theme.of(context).colorScheme.primary,
-                  ),
-                  const SizedBox(width: AppDesign.smallPadding),
-                  GridElement(
-                    title: 'lastTransaction',
-                    futureFunction: ProductDAO().fetchProductsCount(),
-                  ),
-                ],
-              )
             ],
           ),
+          const SizedBox(height: AppDesign.smallPadding),
+          Row(
+            children: [
+              GridElement(
+                title: 'lastBackup',
+                futureFunction: SQLHelper().getLastBackup(),
+                extendedColor: Theme.of(context).colorScheme.primary,
+              ),
+              const SizedBox(width: AppDesign.smallPadding),
+              GridElement(
+                title: 'lastTransaction',
+                futureFunction: ProductDAO().fetchProductsCount(),
+              ),
+            ],
+          )
         ],
       ),
     );
