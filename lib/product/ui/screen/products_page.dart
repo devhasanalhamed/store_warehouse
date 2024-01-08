baff25fcbc563b8d7684eae7c3d9b0c8f41cff77
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-01-08 11:51:56 +0300

    Update products_page.dart

--- lib/product/ui/screen/products_page.dart
+++ lib/product/ui/screen/products_page.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:store_warehouse/core/constants/app_design.dart';
 import 'package:store_warehouse/product/data/product_model.dart';
 import 'package:store_warehouse/product/logic/product_view_model.dart';
 import 'package:store_warehouse/product/ui/widget/product_widget.dart';
@@ -10,18 +11,24 @@ class ProductsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Selector<ProductViewModel, List<ProductModel>>(
-      selector: (_, provider) => provider.productList,
-      builder: (_, productList, __) {
-        if (productList.isNotEmpty) {
-          return ListView.builder(
-            itemCount: productList.length,
-            itemBuilder: (context, index) =>
-                ProductWidget(product: productList[index]),
-          );
-        }
-        return Center(child: Text('empty'.tr()));
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: AppDesign.mediumPadding,
+        vertical: AppDesign.smallPadding,
+      ),
+      child: Selector<ProductViewModel, List<ProductModel>>(
+        selector: (_, provider) => provider.productList,
+        builder: (_, productList, __) {
+          if (productList.isNotEmpty) {
+            return ListView.builder(
+              itemCount: productList.length,
+              itemBuilder: (context, index) =>
+                  ProductWidget(product: productList[index]),
+            );
+          }
+          return Center(child: Text('empty'.tr()));
+        },
+      ),
     );
   }
 }
